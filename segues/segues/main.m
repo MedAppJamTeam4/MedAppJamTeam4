5a3cc0377867ec97f2699c900e9b18f5604c55e5
Van Erick Custodio <erickcustodio@gmail.com>
2012-11-16 12:49:53 -0800

    Added Sample Project named Segues

---
+++ segues/segues/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  segues
+//
+//  Created by Erick Custodio on 11/16/12.
+//  Copyright (c) 2012 Erick Custodio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SegueAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SegueAppDelegate class]));
+    }
+}
